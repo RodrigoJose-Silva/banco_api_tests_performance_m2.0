14925bad7bb53ac7a71aca5147e8a89ad3d49c27
Rodrigo Jose <digo.1903@hotmail.com>
2025-07-12 23:31:11 -0300

    fix(tests): corrigir sintaxe, ponto e vírgula, indentação e linebreak nos testes JS

--- test/login.virtual.users.test.js
+++ test/login.virtual.users.test.js
@@ -1,37 +1,37 @@
 // Import the http module to make HTTP requests. From this point, you can use `http` methods to make HTTP requests.
-import http from 'k6/http'
+import http from 'k6/http';
 // Import the sleep function to introduce delays. From this point, you can use the `sleep` function to introduce delays in your test script.
-import { sleep, check } from 'k6'
+import { sleep, check } from 'k6';
 
 export const options = {
-    // Define the number of iterations for the test
-    vus: 10,
-    duration: '30s',
-    thresholds: {
-        http_req_duration: ['p(95)<2000'],
-        http_req_failed: ['rate<0.01']
-    }
-}
+  // Define the number of iterations for the test
+  vus: 10,
+  duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+    http_req_failed: ['rate<0.01']
+  }
+};
 
 export default function () {
-    const url = 'http://localhost:3000/login'
+  const url = 'http://localhost:3000/login';
 
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    })
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  });
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    const response = http.post(url, payload, params)
+  const response = http.post(url, payload, params);
 
-    check(response, {
-        'Validar que o status é 200': (r) => r.status === 200,
-        'Validar que o token é do tipo string': (r) => typeof (r.json().token) == 'string'
-    })
-    sleep(1)
+  check(response, {
+    'Validar que o status é 200': (r) => r.status === 200,
+    'Validar que o token é do tipo string': (r) => typeof (r.json().token) === 'string'
+  });
+  sleep(1);
 }
